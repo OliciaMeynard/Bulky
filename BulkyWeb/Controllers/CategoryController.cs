be22d6228c5b801020dcda3d3cf5ea953f8e5519
OliciaMeynard <olicia.meynard@gmail.com>
2024-01-05 20:54:40 +0800

    categories add func

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,88 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        ///this is controller method
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        ///this is action method
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();
+            //or use this
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Category categoryObj)
+        {
+            ////this is for custom validation
+            if (categoryObj.Name != null && categoryObj.Name.ToLower() == "test")
+            {
+                //ModelState.AddModelError("Name", "Hindi pwede ang test");
+                ModelState.AddModelError("", "Hindi pwede ang test");
+            }
+
+            //if (categoryObj.Name.ToLower() == null && categoryObj.DisplayOrder.ToString() == null)
+            //{
+            //    ModelState.AddModelError("", "Wag blank! loko");
+            //}
+
+
+            if (ModelState.IsValid)
+            {
+            _db.Categories.Add(categoryObj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+            }
+
+            return View();
+            //if in different controller do this
+            // return RedirectToAction("Index", "differentController");
+        }
+
+
+
+
+
+
+
+
+        //////THIS IS MY JQUERY SAMPLE CODE, THIS IS WORKING
+        //[HttpGet]
+        //public JsonResult GetList()
+        //{
+        //    var objCategoryList = _db.Categories.ToList();
+        //    return new JsonResult(Ok(objCategoryList));
+        //}
+
+
+        //[HttpPost]
+        //public JsonResult PostObject(string name, string job, int age)
+        //{
+
+
+        //    var thename = name;
+        //    var thejob = job;
+        //    var theage = age;
+        //    return new JsonResult(Ok(name));
+        //}
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,15 @@
+using BulkyWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+///creating DB
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+///creating DB the run on tools Nuget Package mngr console command "update-database"
+
 
 var app = builder.Build();
 
