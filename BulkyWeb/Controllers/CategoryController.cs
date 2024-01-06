98e4297d030efaf24eccbf2caf1348f85c115c9e
OliciaMeynard <olicia.meynard@gmail.com>
2024-01-06 17:56:29 +0800

    crud for categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -41,7 +41,7 @@ namespace BulkyWeb.Controllers
 
             //if (categoryObj.Name.ToLower() == null && categoryObj.DisplayOrder.ToString() == null)
             //{
-            //    ModelState.AddModelError("", "Wag blank! loko");
+            //    ModelState.AddModelError("", "Please fill required fields");
             //}
 
 
@@ -49,13 +49,112 @@ namespace BulkyWeb.Controllers
             {
             _db.Categories.Add(categoryObj);
             _db.SaveChanges();
+             TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
 
             }
 
-            return View();
             //if in different controller do this
             // return RedirectToAction("Index", "differentController");
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //ways to retrieve data record
+            //1
+            //Category categoryFromDb = _db.Categories.Find(id);
+
+            //2 //question mark is for nullable it's optional
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(record => record.Id == id);
+
+            //3
+            //Category? categoryFromDb = _db.Categories.Where(cat => cat.Id == id).FirstOrDefault();
+
+
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Category categoryObj)
+        {
+
+            ////this is for custom validation
+            if (categoryObj.Name != null && categoryObj.Name.ToLower() == "test")
+            {
+                //ModelState.AddModelError("Name", "Hindi pwede ang test");
+                ModelState.AddModelError("", "Hindi pwede ang test");
+            }
+
+            //if (categoryObj.Name.ToLower() == null && categoryObj.DisplayOrder.ToString() == null)
+            //{
+            //    ModelState.AddModelError("", "Wag blank! loko");
+            //}
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(categoryObj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+
+    
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+            
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+
+            Category categoryObj = _db.Categories.Find(id);
+            if(categoryObj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(categoryObj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
 
 
